537cf4e6dc2d5e25c0a5ee04a876f3ee890ddf9a
SuYuan <sy94898@163.com>
2025-12-27 14:16:28 +0800

    后台框架

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,10 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 let routes: any = [
-  { path: '/test', name: '测试', component: () => import('@/views/test.vue') },
-  { path: '/login', name: '登录', component: () => import('@/views/login.vue') },
-  { path: '/dashboard', name: '后台', component: () => import('@/views/dashboard/index.vue') },
-]
+  { path: "/test", name: "测试", component: () => import("@/views/test.vue") },
+  { path: "/login", name: "登录", component: () => import("@/views/login.vue"),
+  },
+  {
+    path: "/dashboard", name: "后台", component: () => import("@/views/dashboard/index.vue"),
+    children: [
+      { path: "/dashboard/articles", name: "文章管理", component: () => import("@/views/dashboard/articles.vue")},
+      { path: "/dashboard/categories", name: "分类管理", component: () => import("@/views/dashboard/categories.vue")},
+    ]
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(),
--- client/src/views/dashboard/index.vue
+++ client/src/views/dashboard/index.vue
@@ -1,16 +1,81 @@
 <template>
-    <div>
-      dashboard
-      {{ admin }}
+  <div class="title">后台管理</div>
+    <div class="main-panel">
+      <div class="menus">
+        <div 
+          v-for="(item, index) in menus" 
+          :key="index" 
+          @click="toPage(item.href)"
+        >
+          {{ item.name }}
+        </div>
+      </div>
+      <div style="padding: 20px; width: 100%;">
+        <router-view />
+      </div>
     </div>
 </template>
   
 <script setup lang='ts'>
-import { adminStore } from '@/stores/adminStore'
+import { useRouter, useRoute } from 'vue-router'
+import { onMounted } from 'vue'
 
-const admin = adminStore()
+const router = useRouter()
+const route = useRoute()
+
+const menus = [
+  { name: '文章管理', href: '/dashboard/articles' },
+  { name: '分类管理', href: '/dashboard/categories' },
+  { name: '退出', href: 'logout' },
+]
+
+onMounted(() => {
+  if (!route.path || route.path === '/dashboard') {
+    router.push('/dashboard/articles')
+  }
+})
+
+const toPage = (href: string) => {
+  if (href === 'logout') {
+    router.push('/login')
+  } else {
+    router.push(href)
+  }
+}
 </script>
   
 <style scoped>
-  
+.main-panel {
+  display: flex;
+  color: #64676a;
+  max-width: 1500px;
+  margin: 0 auto;
+}
+
+.title {
+  width: 1500px;
+  font-size: 24px;
+  font-weight: bold;
+  color: #64676a;
+  margin: 0 auto;
+  text-align: center;
+  background: #dadada;
+}
+
+.menus {
+  padding: 20px 0;
+  box-sizing: border-box;
+  line-height: 55px;
+  text-align: center;
+  width: 180px;
+  height: 95vh;
+  border: 1px solid #dadada;
+
+  div {
+    cursor: pointer;
+    &:hover {
+      color: #fd760e;
+    }
+  }
+}
 </style>
